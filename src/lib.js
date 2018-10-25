9044bbe0f834a95bb168290afffbf9ef96e4de70
praathul <22594358+athulpkr98@users.noreply.github.com>
2018-10-25 11:50:44 +0530

    Create functions makeCounterFromZero and makeDeltaTracker
    Created function makeCounterFromZero which returns a counter which
    starts counting from zero.
    
    Created function makeDeltaTracker which will return a function which
    tracks delta.

--- src/lib.js
+++ src/lib.js
@@ -9,8 +9,22 @@ const makeCounterFromN = function(counter){
     return counter++;
   }
 }
-const makeCounterFromZero = undefined;
-const makeDeltaTracker = undefined;
+
+const makeCounterFromZero = function(){
+  return makeCounterFromN(0);
+}
+
+const makeDeltaTracker = function(deltaStatus){
+  let deltaState = {old : deltaStatus, delta : 0, new : deltaStatus};
+  return function(currentDelta){
+    if(currentDelta){
+      deltaState.old = deltaState.new;
+      deltaState.delta = currentDelta;
+      deltaState.new = deltaState.old + currentDelta;
+    }
+    return deltaState;
+  }
+}
 const makeFiboGenerator = undefined;
 const makeCycler = undefined;
 const curry = undefined;
